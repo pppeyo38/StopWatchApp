771a2fe20ce310b421aafd9dc78cf8fdf921b209
pppeyo38 <fuwafuwa.melon47@gmail.com>
2023-01-27 19:01:44 +0900

    ➕ Date Components Formatter

---
+++ Timer/TimerFormat.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+class TimerFormatter: Formatter {
+  let componentFormatter: DateComponentsFormatter = {
+    let formatter = DateComponentsFormatter()
+    formatter.allowedUnits = [.hour, .minute, .second]
+    formatter.zeroFormattingBehavior = .pad
+    return formatter
+  }()
+
+  override func string(for obj: Any?) -> String? {
+    guard let time = obj as? TimeInterval else { return nil }
+
+    guard let formattedString = componentFormatter.string(from: time)  else {
+      return nil
+    }
+
+    let hunredths = Int((time.truncatingRemainder(dividingBy: 1)) * 100)
+    let decimalSperator = Locale.current.decimalSeparator ?? "."
+    return String(format: "%@%@%0.2d", formattedString, decimalSperator, hunredths)
+  }
+
+}
